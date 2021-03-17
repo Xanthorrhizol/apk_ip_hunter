d9d63268a09de64ea4bebcc421cb0f46667ade5d
JuyunLee <45154504+JuyunLee@users.noreply.github.com>
2021-03-17 17:02:23 +0900

    install directory changed
    /usr/bin to /usr/local/bin

--- apk_ip_hunter-install.bash
+++ apk_ip_hunter-install.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$USER" = "root" ]; then
-	cp apk_ip_hunter /usr/bin/apk_ip_hunter
-	chmod +x /usr/bin/apk_ip_hunter
+	cp apk_ip_hunter /usr/local/bin/apk_ip_hunter
+	chmod +x /usr/local/bin/apk_ip_hunter
 	echo -e "\ninstall completed\nUsage: apk_ip_hunter <apk file>\n\n"
 else
 	echo -e "\nPlease run with root privilege\n\n"
